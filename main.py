cc25f5a5159507916884bdcc8cad2ab0dc603bff
glenn <sctglenn@gmail.com>
2020-04-08 23:13:07 +0100

    add while in main add print in main

--- main.py
+++ main.py
@@ -3,38 +3,95 @@ import mysql.connector
 from database import DbManager
 from api import *
 from user import *
-from function_main import category_menu
+
 
 db = Category()
 save = Save()
-"""
-main_loop = True
-
 
 def main():
 
     main_loop = True
 
     while main_loop:
-        print("\nMENU > Entrer le chiffre correspondant à votre choix. ")
-        print("\n1 - Quel aliment souhaitez-vous remplacer ? ")
-        print("2 - Retrouver mes aliments substitués ")
+        menu_presentation()
 
         choice = int(input("\n Votre choix: "))
         if choice == 1:
+            print('\n ------------------ Choisissez une categories -------------------')
             categories = db.show_category()
+            for id, category in categories:
+                print(id, "-", category)
             category_menu(categories)
 
         elif choice == 2:
-            save.show_new_products()
+            saved_product = save.show_new_products()
+            if not saved_product:
+                print("\nVous n'avez rien enregistré.")
+
+            for product_name, substitutes_name in zip(saved_product, saved_product):
+                print(product_name[0], "a été remplacé par", substitutes_name[0], ".")
 
         else:
             print("Erreur : Entrer un numéro valide.")
 
 
+def category_menu(number_category):
+    category_loop = True
+
+    while category_loop:
+        category_choice = input("\nEntrez le numéro de la catégorie: ")
+        if category_choice in number_category:
+            product_menu()
+
+        else:
+            print("Erreur : Entrer un numéro valide.")
+
+
+def product_menu():
+    print('\n ------------------ Choisissez un aliment à remplaçer -------------------')
+    products = db.show_product()
+    x = 1
+    print("Voici la sélection de produits : ")
+    for result in products:
+        print(x, ':', result[0])
+        x += 1
+
+    choices = input("\nEntrez le numéro de l'aliment : ")
+    product_chosen = db.product_chosen(choices)
+    for name in product_chosen:
+        print("\nle produit choisis est :", name[0])
+
+    if int(choices) <= len(products):
+        proposition = save.proposition()
+        random_product = random.choice(proposition)
+        print('ID du produit de substitution: ', random_product[0], '\n',
+              'Pour remplacer ce produit, nous vous proposons : ', random_product[1], '\n',
+              'La description de ce produit est : ', random_product[3], '\n',
+              'Il est disponible dans le(s) magasin(s) suivant(s) : ', random_product[4], '\n',
+              'Son url est la suivante : ', random_product[2])
+
+        save_presentation()
+
+        choice = input("\nVotre choix : ")
+        if choice == "1":
+            id_substitut = input("Entrer l'ID du substitut afin de l'enregistrer:  ")
+            save.save_menu(choices, id_substitut)
+
+        elif choice == "2":
+            categories = db.show_category()
+            category_menu(categories)
+
+        else:
+            print("Erreur : Entrer un numéro valide.")
+            product_menu()
+
+    else:
+        print("Erreur : Entrer un numéro valide.")
+        product_menu()
+
 
 if __name__ == '__main__':
-    main()"""
+    main()
 
-save.proposition()
+
 
--- user.py
+++ user.py
@@ -7,15 +7,10 @@ from api import *
 class Category(DbManager):
 
     def show_category(self):
-        self.mycursor.execute(""" SELECT id, category_name from category WHERE id ORDER BY id LIMIT 10 ; """)
+        self.mycursor.execute(""" SELECT id, category_name from category WHERE id ORDER BY id LIMIT 6 ; """)
         results = self.mycursor.fetchall()
-        categories_id = []
 
-        for _id, _category in results:
-            categories_id.append(str(_id))
-            print(_id,"-", _category)
-
-        return categories_id
+        return results
 
     def show_product(self):
         self.mycursor.execute("""SELECT product_name_fr, code \
@@ -26,28 +21,22 @@ class Category(DbManager):
                 Category.id = products_categories_key.category_id \
                 WHERE category_id  LIMIT 15  """)
         myresult = self.mycursor.fetchall()
-        x = 1
-        print("Voici la sélection de produits : ")
-        for result in myresult:
-            print(x, ':', result[0])
-            x += 1
+
         return myresult
 
-    def product_chosen(self, id):
+    def product_chosen(self, _id):
         self.mycursor.execute("""SELECT product_name_fr \
                         FROM Products \
                         INNER JOIN products_categories_key\
                         ON Products.code = products_categories_key.product_id \
                         INNER JOIN Category ON \
                         Category.id = products_categories_key.category_id \
-                        WHERE category_id =%s  LIMIT 15 """,(id,))
+                        WHERE category_id =%s  LIMIT 15 """,(_id,))
         myresult = self.mycursor.fetchall()
-        for name in myresult:
-            print("\nle produit choisis est :", name[0])
+        return myresult
 
 
 class Save(DbManager):
-
 
     def proposition(self):
 
@@ -63,13 +52,8 @@ class Save(DbManager):
                     ON Store.id = products_stores.store_id \
                     WHERE category_id  ORDER BY RAND() LIMIT 30 """)
         myresult = self.mycursor.fetchall()
-        random_product = random.choice(myresult)
-        print('ID du produit de substitution: ', random_product[0], '\n',
-              'Pour remplacer ce produit, nous vous proposons : ', random_product[1], '\n',
-              'La description de ce produit est : ', random_product[3], '\n',
-              'Il est disponible dans le(s) magasin(s) suivant(s) : ', random_product[4], '\n',
-              'Son url est la suivante : ', random_product[2])
 
+        return myresult
 
     def save_menu(self,product_id,id_substitut):
 
@@ -109,19 +93,8 @@ class Save(DbManager):
                             """, )
         substitute = self.mycursor.fetchall()
 
-        if not product:
-            print("\nVous n'avez rien enregistré.")
-
-        for product_name, substitutes_name in zip(product, substitute):
-            print(product_name[0], "a été remplacé par", substitutes_name[0],".")
+        return substitute, product
 
 
-def main():
 
-    db = Category()
-    db.show_product(4)
-
-
-if __name__ == '__main__':
-    main()
 
